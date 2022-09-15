4ca7c7f2c0434971b4ab51ae2aa16c4deb4c9712
Aaron-Matthew-Go-Tan <96170661+Aaron-Matthew-Go-Tan@users.noreply.github.com>
2022-09-15 11:31:22 +0800

    Add files via upload

---
+++ ex02muontan/Ex02MuonTan.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ex02muontan;
+
+/**
+ *
+ * @author Student
+ */
+import java.util.Scanner;
+public class Ex02MuonTan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          Scanner sc = new Scanner(System.in);
+        //Variables
+        int counter = 0;
+        int guess = 0;
+        int random = (int) Math.floor(Math.random()*10) + 1;
+        
+        // Initial interface
+        System.out.println("Welcome to Higher or Lower! What will you do? (Type s or e)\n" +
+            "s - Start game\n" +
+            "e - End application\n");
+            char startEnd = sc.next().charAt(0);
+            
+        //Game proper
+        while (startEnd == 's') {
+            System.out.printf("You have %d guess(es) left. What is your guess?%n", counter);
+            guess = sc.nextInt();
+            
+            switch (guess) {
+                case random:
+                
+            }
+            /*System.out.println("hi.");
+            startEnd = 'e';*/
+            }
+    }
+}
