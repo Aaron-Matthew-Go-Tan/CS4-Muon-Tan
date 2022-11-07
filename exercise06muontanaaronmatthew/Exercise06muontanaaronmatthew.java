3e2a237829d66ce334f9eb4caaf3842540f9865e
Aaron-Matthew-Go-Tan <96170661+Aaron-Matthew-Go-Tan@users.noreply.github.com>
2022-11-07 08:56:23 +0800

    Add files via upload

---
+++ exercise06muontanaaronmatthew/Exercise06muontanaaronmatthew.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exercise06muontanaaronmatthew;
+
+/**
+ *
+ * @author MUON
+ */
+public class Exercise06muontanaaronmatthew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
